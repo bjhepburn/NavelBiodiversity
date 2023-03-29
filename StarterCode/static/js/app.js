2b178ceb8701412c3bb475022bebb2c942bee715
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-03-28 22:10:06 -0500

    EOD 2023-03-28

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,7 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-let names = []
+let id = d3.select("#selDataset")
+
 navelData = d3.json(url)
 navelData.then((data => {
 
@@ -16,22 +17,59 @@ navelData.then((data => {
         element.value = option;
         select.appendChild(element);
     }
+    
+     
+    d3.select("#selDataset").on("change", updateData);
+    function updateData() {
+        // Update Demographic Info
+        let dropdownMenu = d3.select("#selDataset");
+        let dataset = dropdownMenu.property("value");
 
-    let id = '';
-    // d3.selectAll("#selDataSet").on("change", optionChanged);
-
-    function optionChanged(val) {
-        id = val;
-        document.getElementById("sample-metadata").innerHTML = "ID: " + id;
-    }
+        metadataPos = 0
+        for (let j = 0; j < metadata.length; j++){
+            if (metadata[j].id == dataset) {
+                metadataPos = j;
+            }
 
 
-    for (let j = 0; j < metadata.length; j++){
-        if (metadata[j].id == id) {
+
+        let displayData = document.getElementById("sample-metadata");
+        let ul = document.createElement("ul");
+              
+        for (let j = 0; j < metadata.length; j++){
+        if (metadata[j].id == dataset) {
             for (key in metadata[j]) {
-                console.log(`${key}: ${metadata[j][key]}`);
-            };
-        };
+                displayData.innerHTML = "";
+                let li = document.createElement("li");
+                li.appendChild(document.createTextNode(`${key}: ${metadata[j][key]}`));
+                ul.appendChild(li);
+                };
+            }
+        }
+        displayData.appendChild(ul);
+
+        //Update Bar Chart
+        for (let k = 0; k < samples.length; k++) {
+            if (samples[k].id == dataset) {
+                let y = samples[k].otu_ids.slice(0,10);
+                let x = samples[k].sample_values.slice(0,10);
+                // console.log(samples[k].otu_ids.slice(1,10));
+                let trace = {
+                    x: x,
+                    y: y,
+                    text: samples[k].otu_labels,
+                    type: "bar",
+                    orientation: 'h'
+                }
+            
+            }
+        }    
+    Plotly.newPlot("bar", trace);
     }
 
+    
 }));
+
+function optionChanged(value) {
+        // console.log(value);      
+    }
